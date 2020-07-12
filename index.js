d0896f1dcbc88a94ef9e2fa7e3096c63f7626dee
Victoria Rybalko <rybalko.victoria@gmail.com>
2020-07-12 16:56:49 +0300

    Add class RangeValidator and an example of usage of the class

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 'use strict';
+
+class RangeValidator {
+    constructor(from, to) {
+        this._from = from;
+        this._to = to;
+    }
+
+    set from(value) {
+        if (typeof value !== 'number') {
+            throw new TypeError('Property \'from\' must be number type');
+        }
+        this._from = value;
+    }
+
+    get from() {
+        return this._from;
+    }
+
+    set to(value) {
+        if (typeof value !== 'number') {
+            throw new TypeError('Property \'to\' must be number type');
+        }
+        if (value < this.from) {
+            throw new RangeError('\'Property \'to\' must be bigger than or equal to property \'from\'');
+        }
+        this._to = value;
+    }
+
+    get to() {
+        return this._to;
+    }
+
+    get range() {
+        return [this.from, this.to];
+    }
+
+    validate(number) {
+        return (number >= this.from && number <= this.to);
+    }
+
+}
+
+const range1 = new RangeValidator(-44.5, 24353431);
+try {
+    range1.to = -100;
+} catch (e) {
+    console.error(e);
+}
+console.log(range1.range);
+console.log(range1.validate(444444000002));
+console.log(range1.validate(0));
+console.log(range1.validate(-5));
+
